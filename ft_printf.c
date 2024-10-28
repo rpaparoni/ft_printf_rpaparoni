993f68755a677b3adfa89a87f071d7581371027b
rpaparon <paparoniricardo@gmail.com>
2024-10-28 19:11:30 +0100

    v2

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,33 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:43:49 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/28 18:26:11 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/28 18:58:29 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	*ft_printf(char const *format, ...)
+int	*ft_type(const char c, va_list value, int *counter)
 {
 	
 }
-/*
-#include <stdio.h>
 
-int main()
+int	*ft_printf(char const *str, ...)
 {
-	char *s = "hola pepe";
-	int n = 42;
-	char c = 'k';
+	int		counter;
+	va_list	args;
 
-	printf("%d\n", n);
-	printf("%d\n", c);
-	printf("%s", s);
-}*/
+	counter = 0;
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str != '%')
+			counter += ft_putchar(str[0]);
+		else
+			ft_type(*str++, args, &counter);
+		str++;
+	}
+	i = 0;
+	ve_end(args);
+	return(counter);
+}
--- libftprintf.h
+++ libftprintf.h
@@ -6,13 +6,15 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:40:29 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/28 17:43:53 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/28 19:11:20 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-int   *ft_printf(char const *);
+#include <cstdarg>
+
+int   *ft_printf(char const *format, ...);
 
 #endif
