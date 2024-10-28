5ffc8f4960fd9719ef094b85396fdcc71447e67d
rpaparon <paparoniricardo@gmail.com>
2024-10-28 18:26:22 +0100

    beta

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,26 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:43:49 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/28 17:47:07 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/28 18:26:11 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libftprintf.h>
+#include "libftprintf.h"
 
-int	*ft_printf(char const *)
+int	*ft_printf(char const *format, ...)
 {
 	
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+	char *s = "hola pepe";
+	int n = 42;
+	char c = 'k';
+
+	printf("%d\n", n);
+	printf("%d\n", c);
+	printf("%s", s);
+}*/
