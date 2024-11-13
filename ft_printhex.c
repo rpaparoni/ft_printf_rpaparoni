ee6ae1fbcd20ce4f11cafe26f1611ab20fcc8c79
Ricardo <paparoniricardo@gmail.com>
2024-11-13 13:26:49 +0100

    v7

--- ft_printhex.c
+++ ft_printhex.c
@@ -6,7 +6,7 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:51:36 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/11/13 13:08:08 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/11/13 13:26:44 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ void	ft_puthex(unsigned int num, const char c)
 void	ft_printhex(unsigned int value, const char c, int *counter)
 {
 	if (value == 0)
-		(*counter) += (write(1, "(nill)", 6));
+		(*counter) += (write(1, "0", 1));
 	else
 	{
 		ft_puthex(value, c);
