3fb11274a991e4c981b81d7aab890b7204b94bb8
Ricardo <paparoniricardo@gmail.com>
2024-11-13 14:07:57 +0100

    v8

--- ft_printp.c
+++ ft_printp.c
@@ -6,7 +6,7 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:06:03 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/11/13 13:39:56 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/11/13 14:07:47 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,11 @@ void	ft_printp(unsigned long long value, int *counter)
 	void	*ptr;
 
 	ptr = &value;
-	(*counter) += write(1, "0x", 2);
 	if (ptr == 0)
 		(*counter) += (write(1, "(nil)", 6));
 	else
 	{
+		(*counter) += (write(1, "0x", 2));
 		ft_putptr(value);
 		(*counter) += ft_ptrlen(value);
 	}
